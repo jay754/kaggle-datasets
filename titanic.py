75df42bb0548e271deefb11c71db7ee1cb56b4da
Jay Engineer <jayengineer@Jays-MacBook-Pro-2.local>
2023-08-25 19:05:42 -0400

    added some visulizations

--- titanic.py
+++ titanic.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import sklearn
+import seaborn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 from sklearn import metrics
 
 titanic_data = pd.read_csv('titanic_train.csv')
@@ -13,7 +19,11 @@ print(titanic_data.describe())
 
 print(titanic_data[["Sex", "Survived"]].groupby(['Sex'], as_index=False))
 
-print(train.isnull().sum())
+print(titanic_data.isnull().sum())
+
+print(seaborn.countplot (x ="Survived", data = titanic_data))
+
+print(titanic_data["Age"].hist())
 
 titanic_data.drop('Cabin', axis=1, inplace = True)
 titanic_data.drop('Fare', axis=1, inplace = True)
